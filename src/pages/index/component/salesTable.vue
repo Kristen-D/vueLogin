396aaa1a39bf9cd7540fd5cf72d185d0dbf49e4f
donglongting <donglongting@inspur>
2019-12-31 10:50:07 +0800

    index页面上面的小框完成

---
+++ src/pages/index/component/salesTable.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="salesTable">
+    <el-table>
+      <el-table-column>
+
+      </el-table-column>
+      <el-table-column>
+
+      </el-table-column>
+      <el-table-column class-name="salesStatus"
+                       prop="status"
+                       label="STATUS">
+        <template slot-scope="scope">
+          <span v-if="scope.row.status === 1" class="saleBgcolor">SALE</span>
+          <span v-if="scope.row.status === 2" class="taxBgcolor">TAX</span>
+          <span v-if="scope.row.status === 3" class="extenedBgcolor">EXTENDED</span>
+          <span v-if="scope.row.status === 4" class="likeBgcolor">LIKE</span>
+        </template>
+      </el-table-column>
+
+    </el-table>
+  </div>
+</template>
+
+<script>
+  import userImg from "@/assets/img/avatar-3.png";
+  export default {
+    data() {
+      return {
+        tableData: [],
+        userImg: userImg
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,76 @@
 <template>
   <section class="data_section" ref="data_section">
+    <a :href="github" target="_blank">
+      <el-row :gutter="10" class="row_list">
+        <el-col :span="8">
+          <div class="row_left row_base">
+            <el-col :span="12">
+              <div class="pay saleBgcolor data_list rflex">
+                <div class="leftItem cflex wflex">
+                  <p class="investor">{{ $t('index.yearLoss') }}</p>
+                  <p class="number">68<span class="perTitle">({{ $t('index.tenMillion') }})</span></p>
+                </div>
+                <div class="rightItem">
+                  <icon-svg icon-class="iconfukuan" />
+                </div>
+              </div>
+            </el-col>
+            <el-col :span="12">
+              <div class="pay taxBgcolor data_list rflex">
+                <div class="leftItem cflex wflex">
+                  <p class="investor">{{ $t('index.yearProfit') }}</p>
+                  <p class="number">120<span class="perTitle">(%)</span></p>
+                </div>
+                <div class="rightItem">
+                  <icon-svg icon-class="iconpay3" />
+                </div>
+              </div>
+            </el-col>
+          </div>
+        </el-col>
+        <el-col :span="16">
+          <div class="row_right row_base">
+            <el-col :span="6">
+                <div class="extenedBgcolor data_list rflex">
+                  <div class="leftItem cflex wflex">
+                    <p class="investor">{{ $t('index.potentialInvestor') }}</p>
+                    <p class="number">686<span class="perTitle">({{ $t('index.person') }})</span></p>
+                    <p class="cardText">Decreased by 10%</p>
+                  </div>
+                </div>
+            </el-col>
+            <el-col :span="6">
+              <div class="likeBgcolor data_list rflex">
+                <div class="leftItem cflex wflex">
+                  <p class="investor">{{ $t('index.intentionInvestor') }}</p>
+                  <p class="number">1276<span class="perTitle">({{ $t('index.person') }})</span></p>
+                  <p class="cardText">Decreased by 50%</p>
+                </div>
+              </div>
+            </el-col>
+            <el-col :span="6">
+              <div class="linkBgColor data_list rflex">
+                <div class="leftItem cflex wflex">
+                  <p class="investor">{{ $t('index.waitExamineInvestor') }}</p>
+                  <p class="number">82367<span class="perTitle">({{ $t('index.person') }})</span></p>
+                  <p class="cardText">Decreased by 30%</p>
+                </div>
+              </div>
+            </el-col>
+            <el-col :span="6">
+              <div class="keleBgColor data_list rflex">
+                <div class="leftItem cflex wflex">
+                  <p class="investor">{{ $t('index.examiningInvestor') }}</p>
+                  <p class="number">827373<span class="perTitle">({{ $t('index.person') }})</span></p>
+                  <p class="cardText">Decreased by 80%</p>
+                </div>
+              </div>
+            </el-col>
+          </div>
+        </el-col>
+      </el-row>
+    </a>
+
     <el-row :gutter="10" class="row_list order_list">
       <el-col :span="7">
         <log-lists></log-lists>
@@ -7,6 +78,12 @@
      <el-col :span="17">
 				<comment-list></comment-list>
 			</el-col>
+    </el-row>
+
+    <el-row :gutter="10" class="row_list">
+      <el-col :span="11">
+        <sales-table></sales-table>
+      </el-col>
     </el-row>
   </section>
 </template>
